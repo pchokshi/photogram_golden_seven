2cedc6839170b6d176fc4fdcbac970cd55b8be4d
pchokshi <poojachokshi@Poojas-MacBook-Air.local>
2015-10-29 20:27:09 -0500

    done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -26,4 +26,15 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
   end
 
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+  def update_row
+    @photo=Photo.find_by({ :id => params[:id]})
+    @photo.caption = params[:the_caption]
+    @photo.source = params[:the_source]
+    @photo.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
   get("/delete_photo/:id", { :controller => "photos", :action => "delete_row" })
+
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 end
