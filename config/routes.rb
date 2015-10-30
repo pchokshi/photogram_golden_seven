4404cd334068378d31a571e1f5ab3e151ce01782
pchokshi <poojachokshi@Poojas-MacBook-Air.local>
2015-10-29 19:40:11 -0500

    delete done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Routes to CREATE photos
+  get("/photos/new", {:controller => "photos", :action => "new_form"})
+  get("/create_photo", {:controller => "photos", :action => "create_row"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  get("/delete_photo/:id", { :controller => "photos", :action => "delete_row" })
 end
